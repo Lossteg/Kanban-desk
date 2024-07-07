fb285ab2044c1f90e72c67757c1e3c385892fd7b
Lossteg <94453559+Lossteg@users.noreply.github.com>
2024-07-07 13:42:04 +0300

    feat: add Swagger setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Kanban-desk')
+    .setDescription('Kanban-desk API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = Number(process.env.PORT) || 3000;
   console.log(port);
