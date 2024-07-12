60632e65c4beeeaa3111d429a1f6bc94c20e91a0
Lossteg <94453559+Lossteg@users.noreply.github.com>
2024-07-12 14:15:34 +0300

    feat: add some styles to components

--- client/src/pages/sign-up/ui/index.tsx
+++ client/src/pages/sign-up/ui/index.tsx
@@ -1,13 +1,19 @@
 import { FC } from "react";
 import RegisterForm from "../../../features/auth/ui/Register-form";
-import AuthImg from "../../../assets/9.png";
+import AuthImg from "../../../assets/4406c6e741804bf3e3cefae36f09cc8a.png";
 import "./index.scss";
 
 const SignUpPage: FC = () => {
   return (
     <div className="signup-container">
       <RegisterForm />
-      <img className="signup-img" src={AuthImg} alt="registration-background"/>
+      <div className="signup-img-container">
+        <img
+          className="signup-img"
+          src={AuthImg}
+          alt="registration-background"
+        />
+      </div>
     </div>
   );
 };
